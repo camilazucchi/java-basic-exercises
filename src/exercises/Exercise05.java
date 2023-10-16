805b9e623ac3c09e6e0489501bc3421eb6bd41fc
camilazci <camilazucchi@outlook.com>
2023-10-16 18:05:55 -0300

    feat: add Exercise05

---
+++ src/exercises/Exercise05.java
@@ -0,0 +1,25 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Exercise05 {
+    public static void main(String[] args) {
+        /* 5. Write a Java program that takes two numbers as input and displays the product
+        of two numbers.
+        Test Data:
+        Input first number: 25
+        Input second number: 5
+        Expected Output:
+        25 x 5 = 125
+         */
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, enter the first number: ");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Please, enter the second number: ");
+        int secondNumber = scanner.nextInt();
+        int productOfNumbers = firstNumber * secondNumber;
+        System.out.println(firstNumber + " x " + secondNumber + " = " + productOfNumbers);
+
+        scanner.close();
+    }
+}

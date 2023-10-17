da7fe660fb3914ddc06da573bf4ef19d052a55e4
camilazci <camilazucchi@outlook.com>
2023-10-17 09:29:12 -0300

    feat: add Exercise08

---
+++ src/exercises/Exercise08.java
@@ -0,0 +1,18 @@
+package exercises;
+
+public class Exercise08 {
+    public static void main(String[] args) {
+        /* 8. Write a Java program to display the following pattern.
+        Sample Pattern:
+        J    a   v     v  a
+        J   a a   v   v  a a
+     J  J  aaaaa   V V  aaaaa
+      JJ  a     a   V  a     a
+        */
+        System.out.println("""
+                J    a   v     v  a
+                J   a a   v   v  a a
+             J  J  aaaaa   V V  aaaaa
+              JJ  a     a   V  a     a""");
+    }
+}

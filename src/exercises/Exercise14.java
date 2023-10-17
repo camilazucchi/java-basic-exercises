56de9b487ad563dc9676b92fa002d0e943692573
camilazci <camilazucchi@outlook.com>
2023-10-17 10:11:08 -0300

    feat: add Exercise14

---
+++ src/exercises/Exercise14.java
@@ -0,0 +1,30 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Exercise14 {
+    public static void main(String[] args) {
+        /* 14. Write a Java program to swap to variables.
+        */
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please, enter the first number: ");
+        int inputFirstNumber = scanner.nextInt();
+
+        System.out.println("Please, enter the second number: ");
+        int inputSecondNumber = scanner.nextInt();
+
+        System.out.println("The first number is: " + inputFirstNumber);
+        System.out.println("The second number is: " + inputSecondNumber);
+        System.out.println("Shuffling numbers...");
+
+        int temporaryNumber = inputFirstNumber;
+        inputFirstNumber = inputSecondNumber;
+        inputSecondNumber = temporaryNumber;
+
+        System.out.println("After shuffling... The first number is: " + inputFirstNumber);
+        System.out.println("After shuffling... The second number is: " + inputSecondNumber);
+
+        scanner.close();
+    }
+}

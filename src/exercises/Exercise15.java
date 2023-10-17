349690dbb116483ed1e8da6ae61555b2e3e75616
camilazci <camilazucchi@outlook.com>
2023-10-17 13:02:45 -0300

    feat: add Exercise15

---
+++ src/exercises/Exercise15.java
@@ -0,0 +1,13 @@
+package exercises;
+
+public class Exercise15 {
+    public static void main(String[] args) {
+        /* 15. Write a Java program to print a face.
+        */
+        System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '-' | ");
+        System.out.println(" +-----+ ");
+    }
+}
